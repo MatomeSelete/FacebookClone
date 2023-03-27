9b769b256b334f28c45820b4dc464d70de686654
Matome <seletematome3@gmail.com>
2023-03-27 11:54:13 +0200

    navbar ui visible

--- facebookclone/src/components/NavBar/NavBar.js
+++ facebookclone/src/components/NavBar/NavBar.js
@@ -2,18 +2,34 @@ import React from 'react'
 
 import { GoSearch } from 'react-icons/go';
 import { HiMenu } from 'react-icons/hi';
+import { HiOutlineHome } from 'react-icons/hi';
+import { FaUserFriends } from 'react-icons/fa';
+import { TbMessageCircle } from 'react-icons/tb';
+import { BsFillDisplayFill } from 'react-icons/bs';
+import { IoMdNotificationsOutline } from 'react-icons/io';
+import { HiUserGroup } from 'react-icons/hi';
 
 
 import './NavBar.css'
 
 function NavBar() {
   return (
-    <div>
+    <div className='navbar__page'>
       <div className='topNav' >
-        <h1>facebook</h1>
+        <h1 className='topnav__logo'>facebook</h1>
+        <div className='topnav__icons'>
+          <h2> <GoSearch /></h2>
+          <h2> <HiMenu /></h2>
+        </div>
+      </div>
+      <div className='bottomNav'>
+        <h3><HiOutlineHome /></h3>
+        <h3><FaUserFriends /> </h3>
+        <h3><TbMessageCircle /> </h3>
+        <h3><BsFillDisplayFill /> </h3>
+        <h3><IoMdNotificationsOutline /> </h3>
+        <h3><HiUserGroup /> </h3>
 
-        <GoSearch />
-        <HiMenu />
       </div>
 
     </div>
