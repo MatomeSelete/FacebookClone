3a9ae0772ed7c52188c2deb980fa10fb01c81421
Matome <seletematome3@gmail.com>
2023-03-28 11:39:06 +0200

    like button interracting

--- facebookclone/src/components/Home/Home.js
+++ facebookclone/src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
@@ -10,6 +11,8 @@ import { BsImages } from 'react-icons/bs';
 import { AiOutlineLike } from 'react-icons/ai';
 import { BiMessage } from 'react-icons/bi';
 import { RiShareForwardLine } from 'react-icons/ri';
+import { AiFillLike } from 'react-icons/ai';
+
 
 import './Home.css'
 
@@ -20,6 +23,16 @@ import Ciara from '../images/Ciara.jpeg'
 import Sarra from '../images/Sarra.jpeg'
 
 function Home() {
+
+  const [liked, setLiked] = useState(false);
+
+  const handleNotification = ()=> {
+    setLiked(true)
+  }
+
+  const handleNotificationNot = ()=> {
+    setLiked(false)
+  }
   return (
 
     <>
@@ -92,17 +105,21 @@ function Home() {
         {/*================= NEWS FEED PART  =================*/}
         <div className='newsFeed'>
 
-        <div className='newsfeed__cards'>
+          <div className='newsfeed__cards'>
             <Card >
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Leo</Card.Title>
                 <Card.Text>
                   Some quick example text to build on the card title and make up the
                   bulk of the card's content.
                 </Card.Text>
                 <Card.Img variant="top" src={Leo} />
                 <div className='reaction__button'>
-                <Button variant="primary"> <AiOutlineLike />1.5k </Button>
+                {liked ? (
+                    <Button className='cardIcon' onClick={handleNotificationNot} > <AiOutlineLike /> 1.5k </Button>
+                  ) : (
+                    <Button className='cardIcon' onClick={handleNotification} > <AiFillLike /> 1.6 </Button>
+                  )}
                   <Button variant="primary"> <BiMessage />300 </Button>
                   <Button variant="primary"> <RiShareForwardLine />1k </Button>
                 </div>
@@ -113,14 +130,18 @@ function Home() {
           <div className='newsfeed__cards'>
             <Card >
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Ciara</Card.Title>
                 <Card.Text>
                   Some quick example text to build on the card title and make up the
                   bulk of the card's content.
                 </Card.Text>
                 <Card.Img variant="top" src={Ciara} />
                 <div className='reaction__button'>
-                <Button variant="primary"> <AiOutlineLike />5.9k </Button>
+                {liked ? (
+                    <Button className='cardIcon' onClick={handleNotificationNot} > <AiOutlineLike /> 5.9k </Button>
+                  ) : (
+                    <Button className='cardIcon' onClick={handleNotification} > <AiFillLike /> 6k </Button>
+                  )}
                   <Button variant="primary"> <BiMessage />10.k </Button>
                   <Button variant="primary"> <RiShareForwardLine />2.9k </Button>
                 </div>
@@ -131,14 +152,18 @@ function Home() {
           <div className='newsfeed__cards'>
             <Card >
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Sarra</Card.Title>
                 <Card.Text>
                   Some quick example text to build on the card title and make up the
                   bulk of the card's content.
                 </Card.Text>
                 <Card.Img variant="top" src={Sarra} />
                 <div className='reaction__button'>
-                <Button variant="primary"> <AiOutlineLike />50.8k </Button>
+                {liked ? (
+                    <Button className='cardIcon' onClick={handleNotificationNot} > <AiOutlineLike /> 70k </Button>
+                  ) : (
+                    <Button className='cardIcon' onClick={handleNotification} > <AiFillLike /> 70.1k </Button>
+                  )}
                   <Button variant="primary"> <BiMessage />40.3k </Button>
                   <Button variant="primary"> <RiShareForwardLine />20.5k </Button>
                 </div>
@@ -149,14 +174,18 @@ function Home() {
           <div className='newsfeed__cards'>
             <Card >
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Tic</Card.Title>
                 <Card.Text>
                   Some quick example text to build on the card title and make up the
                   bulk of the card's content.
                 </Card.Text>
                 <Card.Img variant="top" src={Tic} />
                 <div className='reaction__button'>
-                  <Button variant="primary"> <AiOutlineLike />200.3k </Button>
+                  {liked ? (
+                    <Button className='cardIcon' onClick={handleNotificationNot} > <AiOutlineLike /> 700k </Button>
+                  ) : (
+                    <Button className='cardIcon' onClick={handleNotification} > <AiFillLike /> 700k </Button>
+                  )}
                   <Button variant="primary"> <BiMessage />500.6k </Button>
                   <Button variant="primary"> <RiShareForwardLine />100.5k </Button>
                 </div>
