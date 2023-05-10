a7a521631662c988c521e167bd4d2a562c9efb98
Matome <seletematome3@gmail.com>
2023-05-10 13:53:46 +0200

    overflow not working

--- facebookclone/src/components/Message/onlineChats.js
+++ facebookclone/src/components/Message/onlineChats.js
@@ -1,69 +1,75 @@
 import React from "react";
 
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-
-import Ciara from '../images/Ciara.jpeg';
-import  Leo  from '../images/Leo.jpeg'; 
-import  Matome  from '../images/Matome.jpg';
-import  Sarra  from '../images/Sarra.jpeg'; 
-import  tic  from '../images/ticTacToe.png';  
+import Ciara from "../images/Ciara.jpeg";
+import Leo from "../images/Leo.jpeg";
+import Matome from "../images/Matome.jpg";
+import Sarra from "../images/Sarra.jpeg";
+import tic from "../images/ticTacToe.png";
 
 function OnlineChats() {
   return (
-    <div className="container OnlineChats___cards">
+    <div className="container ">
 
-      <div className="card ">
-        <div className="card-body">
-          {/* <img src="../images/Ciara.jpeg"  alt="..." /> */}
-          <img src={Ciara}  alt="..." />
+      <div className="OnlineChats___cards">
+        <div className="card ">
+          <div className="card-body">
+            {/* <img src="../images/Ciara.jpeg"  alt="..." /> */}
+            <img src={Ciara} alt="..." />
+          </div>
         </div>
+
+        <div class="card">
+          <div class="card-body">
+            <p class="card-text">
+              <img src={Leo} class="card-img-top" alt="..." />
+            </p>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-body">
+            <p class="card-text">
+              <img src={Matome} class="card-img-top" alt="..." />
+            </p>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-body">
+            <p class="card-text">
+              <img src={Sarra} class="card-img-top" alt="..." />
+            </p>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-body">
+            <p class="card-text">
+              <img src={Sarra} class="card-img-top" alt="..." />
+            </p>
+          </div>
+        </div>
+
+       {/* <div class="card">
+          <div class="card-body">
+            <p class="card-text">
+              <img src={Leo} class="card-img-top" alt="..." />
+            </p>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-body">
+            <p class="card-text">
+              <img src={Matome} class="card-img-top" alt="..." />
+            </p>
+          </div>
+        </div>
+         */}
       </div>
-
-      
-
-
-
-      
-
-      
-
-      
-
-
-       <div class="card">
-        <div class="card-body">
-          <p class="card-text">
-          <img src={Leo} class="card-img-top" alt="..." />
-          </p>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="card-body">
-          <p class="card-text">
-          <img src={Matome} class="card-img-top" alt="..." />
-          </p>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="card-body">
-          <p class="card-text">
-          <img src={Sarra} class="card-img-top" alt="..." />
-          </p>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="card-body">
-          <p class="card-text">
-          <img src={Sarra} class="card-img-top" alt="..." />
-          </p>
-        </div>
-      </div>{/* */}
-
     </div>
   );
 }
