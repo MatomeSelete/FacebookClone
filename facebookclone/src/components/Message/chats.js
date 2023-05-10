6df64f41bc6102768a0336183ab99efa0c77e1c6
Matome <seletematome3@gmail.com>
2023-05-10 16:20:05 +0200

    overflow for chats and online friends not showing

--- facebookclone/src/components/Message/chats.js
+++ facebookclone/src/components/Message/chats.js
@@ -1,11 +1,104 @@
-import React from 'react'
+import React from "react";
+
+import Ciara from "../images/Ciara.jpeg";
 
 function Chats() {
   return (
-    <div>
-        
+    <div className="container">
+      <div className="chatsCards">
+        <div class="card">
+          <div className="chatimg"> 
+            <img src={Ciara} class="card-img-top" alt="..." />
+          </div>
+
+          <div class="nameText">
+            <h5 class="card-text">Some quick</h5>
+            <p>scrollbar is added to scroll.</p>
+          </div>
+        </div>
+
+        <div class="card">
+          <div className="chatimg"> 
+            <img src={Ciara} class="card-img-top" alt="..." />
+          </div>
+
+          <div class="nameText">
+            <h5 class="card-text">Some quick</h5>
+            <p>scrollbar is added to scroll.</p>
+          </div>
+        </div>
+
+        <div class="card">
+          <div className="chatimg"> 
+            <img src={Ciara} class="card-img-top" alt="..." />
+          </div>
+
+          <div class="nameText">
+            <h5 class="card-text">Some quick</h5>
+            <p>scrollbar is added to scroll.</p>
+          </div>
+        </div>
+
+
+        <div class="card">
+          <div className="chatimg"> 
+            <img src={Ciara} class="card-img-top" alt="..." />
+          </div>
+
+          <div class="nameText">
+            <h5 class="card-text">Some quick</h5>
+            <p>scrollbar is added to scroll.</p>
+          </div>
+        </div>
+
+        <div class="card">
+          <div className="chatimg"> 
+            <img src={Ciara} class="card-img-top" alt="..." />
+          </div>
+
+          <div class="nameText">
+            <h5 class="card-text">Some quick</h5>
+            <p>scrollbar is added to scroll.</p>
+          </div>
+        </div>
+
+        <div class="card">
+          <div className="chatimg"> 
+            <img src={Ciara} class="card-img-top" alt="..." />
+          </div>
+
+          <div class="nameText">
+            <h5 class="card-text">Some quick</h5>
+            <p>scrollbar is added to scroll.</p>
+          </div>
+        </div>
+
+        <div class="card">
+          <div className="chatimg"> 
+            <img src={Ciara} class="card-img-top" alt="..." />
+          </div>
+
+          <div class="nameText">
+            <h5 class="card-text">Some quick</h5>
+            <p>scrollbar is added to scroll.</p>
+          </div>
+        </div>
+
+        <div class="card">
+          <div className="chatimg"> 
+            <img src={Ciara} class="card-img-top" alt="..." />
+          </div>
+
+          <div class="nameText">
+            <h5 class="card-text">Some quick</h5>
+            <p>scrollbar is added to scroll.</p>
+          </div>
+        </div>
+
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default Chats
+export default Chats;
