d6a1b38875b90b17cb8f4ef4f46a8d4328a17561
Matome <seletematome3@gmail.com>
2023-05-10 12:32:46 +0200

    react image not displaying for profile in the inbox

--- facebookclone/src/components/Home/Posts.js
+++ facebookclone/src/components/Home/Posts.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import { useState } from 'react';
-import { Link, useNavigate, useParams } from "react-router-dom";
+// import { useState } from 'react';
+// import { Link, useNavigate, useParams } from "react-router-dom";
 
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
-import Carousel from 'react-bootstrap/Carousel';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+// import Carousel from 'react-bootstrap/Carousel';
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
 
 import { BsImages } from 'react-icons/bs';
-import { AiOutlineLike } from 'react-icons/ai';
-import { BiMessage } from 'react-icons/bi';
-import { RiShareForwardLine } from 'react-icons/ri';
-import { AiFillLike } from 'react-icons/ai';
+// import { AiOutlineLike } from 'react-icons/ai';
+// import { BiMessage } from 'react-icons/bi';
+// import { RiShareForwardLine } from 'react-icons/ri';
+// import { AiFillLike } from 'react-icons/ai';
 
 import Matome from '../images/Matome.jpg'
-import Tic from '../images/ticTacToe.png'
-import Leo from '../images/Leo.jpeg'
-import Ciara from '../images/Ciara.jpeg'
-import Sarra from '../images/Sarra.jpeg'
+// import Tic from '../images/ticTacToe.png'
+// import Leo from '../images/Leo.jpeg'
+// import Ciara from '../images/Ciara.jpeg'
+// import Sarra from '../images/Sarra.jpeg'
 
 import './Home.css'
 
--- facebookclone/src/components/Message/onlineChats.js
+++ facebookclone/src/components/Message/onlineChats.js
@@ -1,11 +1,71 @@
-import React from 'react'
+import React from "react";
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
+import { Ciara } from '../images/Ciara.jpeg';
+import { Leo } from '../images/Leo.jpeg'; 
+import { Matome } from '../images/Matome.jpg';
+import { Sarra } from '../images/Sarra.jpeg'; 
+import { tic } from '../images/ticTacToe.png';  
 
 function OnlineChats() {
   return (
-    <div>
+    <div className="container">
+
+      <div class="card">
+        <div class="card-body">
+          <p class="card-text">
+          <img src="../images/Ciara.jpeg"  alt="..." />
+          </p>
+        </div>
+      </div>
+
         
+      {/* <Card >
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Img variant="top" src="../images/Ciara.jpeg" />
+      </Card.Body>
+    </Card> */}
+
+
+
+      {/* <div class="card">
+        <div class="card-body">
+          <p class="card-text">
+          <img src={Leo} class="card-img-top" alt="..." />
+          </p>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-body">
+          <p class="card-text">
+          <img src={Matome} class="card-img-top" alt="..." />
+          </p>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-body">
+          <p class="card-text">
+          <img src={Sarra} class="card-img-top" alt="..." />
+          </p>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-body">
+          <p class="card-text">
+          <img src={Sarra} class="card-img-top" alt="..." />
+          </p>
+        </div>
+      </div> */}
+
     </div>
-  )
+  );
 }
 
-export default OnlineChats
+export default OnlineChats;
