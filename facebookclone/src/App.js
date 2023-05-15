555aa684863fcb7a4d7c9818ec794fb4cae59b44
Matome <seletematome3@gmail.com>
2023-05-15 11:59:41 +0200

    done with the routes

--- facebookclone/src/App.js
+++ facebookclone/src/App.js
@@ -2,17 +2,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  Home,
-  NavBar,
-  Friends,
-  Shorts,
-  Groups,
-  Notification,
-  Message,
-} from "./components";
+import { Home, NavBar, Friends, Shorts, Groups, Notification, Message } from "./components";
 import { Posts, Stories, NewsFeed, Comments } from "./components/Home";
-import { Messagerearch, OnlineChats, Chats, Inbox } from "./index";
+import { Messagerearch, OnlineChats, Chats, Inbox } from "./components/Message";
 
 import "./App.css";
 
@@ -23,18 +15,13 @@ function App() {
 
       <BrowserRouter>
         <Routes>
-         {/* <Route path="/dash" element={<Navbar />} >
-             <Route path="pack" element={<Pack />}></Route>
-              <Route path="form" element={<Form />}></Route>
 
-              </Route> */}
-
-            <Route path="/" element={<NavBar />}>
-              <Route index element={<Home />} >
-                <Route path="./components/Home/Posts.js" element={<Posts />}  > </Route>
-                <Route path="./components/Home/Stories.js" element={<Stories />}  > </Route>
-                <Route path="./components/Home/NewsFeed.js" element={<NewsFeed />}  > </Route>
-                <Route path="./components/Home/Comments.js" element={<Comments />}  > </Route>
+             <Route path="/" element={<NavBar />}>
+             <Route path="Home" element={<Home />} >
+                <Route path="./components/Home/Posts.js" element={<Posts />}> </Route>
+                <Route path="./components/Home/Stories.js" element={<Stories />}> </Route>
+                <Route path="./components/Home/NewsFeed.js" element={<NewsFeed />}> </Route>
+                <Route path="./components/Home/Comments.js" element={<Comments />}> </Route>
               </Route>  
 
               <Route path="/friends" element={<Friends />} >
@@ -45,11 +32,11 @@ function App() {
 
               </Route>
 
-            <Route path="/message" element={<Message />} >
-              <Route path="./components/Message/messagersearch.js" element={<Messagerearch />}  > </Route>
-              <Route path="./components/Message/OnlineChats.js" element={<OnlineChats />}  > </Route>
-              <Route path="./components/Message/chats.js" element={<Chats />}  > </Route>
-              <Route path="./components/Message/Inbox.js" element={<Inbox />}  > </Route>
+            <Route path="Message" element={<Message />} >
+              <Route path="./components/Message/Messagersearch.js" element={<Messagerearch />}> </Route>
+              <Route path="./components/Message/OnlineChats.js" element={<OnlineChats />}> </Route>
+              <Route path="./components/Message/Chats.js" element={<Chats />}> </Route>
+              <Route path="./components/Message/Inbox.js" element={<Inbox />}> </Route>
             </Route>
 
             <Route path="groups" element={<Groups />} >
