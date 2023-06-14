8325776450280314217dcc3be704b59a4e98e387
Matome <seletematome3@gmail.com>
2023-06-14 16:27:49 +0200

    adding modals

--- facebookclone/src/components/Friends/Birthdays.js
+++ facebookclone/src/components/Friends/Birthdays.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React from "react";
 
 function Birthdays() {
   return (
     <div>
       Birthdays
       <h4>Birthdays</h4>
+      <div class="modal" tabindex="-1">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Modal title</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <p>Modal body text goes here.</p>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div class="modal" tabindex="-1">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -25,7 +56,7 @@ function Birthdays() {
 
 
     </div>
-  )
+  );
 }
 
-export default Birthdays
+export default Birthdays;
