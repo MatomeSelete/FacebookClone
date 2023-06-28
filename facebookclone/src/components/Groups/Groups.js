078b59e04fb7c95d9d35adb724032580cdf305b4
Matome <seletematome3@gmail.com>
2023-06-28 19:39:18 +0200

    group card

--- facebookclone/src/components/Groups/Groups.js
+++ facebookclone/src/components/Groups/Groups.js
@@ -4,7 +4,21 @@ import './Groups.css'
 
 function Groups() {
   return (
-    <div>Groups</div>
+    <div>
+      <div className='newsfeed__cards'>
+        
+        <Card >
+          <Card.Body>
+            <Card.Title>Leo</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Card.Img variant="top" src={Leo} />
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
   )
 }
 
